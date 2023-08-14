c2a215ad43ee6b306747611ff646465d5f23b372
Mardon1207 <mardonsultonov8@gmail.com>
2023-08-14 11:46:32 +0500

    bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+from flask import Flask,request
+import requests
+app = Flask(__name__)
+@app.route("/bot")
+def hello():
+    if request.method=="POST":
+        data=request.get_json()
+        chat_id=data["message"]["chat"]["id"]
+        url="https://api.telegram.org/bot6089863461:AAGuL8gKw5QdjXaqwcNY6dZmQmezYVgtDUg/sendMessage"
+        payload={
+            "chat_id":chat_id,
+            "text":data["message"]['text']
+        }
+        return requests.get(url,payload)
