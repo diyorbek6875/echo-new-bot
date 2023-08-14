5e2db9de69756beb94c354e8b7501ea22bf7e25a
Mardon1207 <mardonsultonov8@gmail.com>
2023-08-14 17:28:58 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from flask import Flask,request
 import requests,telegram
 app = Flask(__name__)
-@app.route("/bot/")
+@app.route("/bot/", methods=['POST', 'GET'])
 def bot():
     if request.method=="POST":
         data=request.get_json()
