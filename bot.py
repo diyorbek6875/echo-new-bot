c3a7cbd32c14f59e1a491c07090ef89101e5b7e3
Mardon1207 <mardonsultonov8@gmail.com>
2023-08-14 17:09:20 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -3,18 +3,15 @@ import requests,telegram
 app = Flask(__name__)
 @app.route("/bot")
 def bot():
-    telegram.Bot.get_webhook_info()
-    telegram.Bot.delete_webhook()
-    telegram.Bot.set_webhook(url="https://mardon.pythonanywhere.com/bot")
     if request.method=="POST":
         data=request.get_json()
         chat_id=data["message"]["chat"]["id"]
-        url1="https://api.telegram.org/bot6190918955:AAED-3niA2qqmsmDZmWFAEDcLKRmmys9eTs/sendMessage"
+        url="https://api.telegram.org/bot6190918955:AAED-3niA2qqmsmDZmWFAEDcLKRmmys9eTs/sendMessage"
         payload={
             "chat_id":chat_id,
             "text":"salom"
         }
-        requests.get(url=url1,params=payload)
+        requests.get(url,params=payload)
         return {"message":"ok"}
 if __name__ == "__main__":
     app.run(debug=True)
