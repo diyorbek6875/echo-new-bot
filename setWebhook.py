8d0dd5b74fc1ad397923a301add47f9a04b27f0b
Diyorbek <129488800+diyorbek6875@users.noreply.github.com>
2023-08-18 14:59:57 +0500

    Update setWebhook.py

--- setWebhook.py
+++ setWebhook.py
@@ -1,6 +1,6 @@
 import telegram
 url="https://mardon.pythonanywhere.com/bot/"
-TOKEN="6190918955:AAED-3niA2qqmsmDZmWFAEDcLKRmmys9eTs"
+TOKEN="6618036571:AAEfUwyhzOTSCdDWhBMIryuyDkd5lQYoiSQ"
 bot=telegram.Bot(token=TOKEN)
 bot.delete_webhook()
 print(bot.set_webhook(url))
